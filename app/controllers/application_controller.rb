e36b2938b7ca125324636e380203566622dda702
Diego Aguilar <diego.a.mazzero@gmail.com>
2017-10-23 20:32:02 -0700

    Checkpoint 4: Create user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,14 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource_or_scope)
+    users_show_path
+  end
+
   def configure_permitted_parameters
     added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
-  
+
 end
